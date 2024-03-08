9335d7ef1978cbabd4239cd7b4716358659387ce
bekzhanJS <bekzhanzhumassatov@gmail.com>
2024-03-08 19:41:20 +0500

    added add task functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+let input = document.getElementById("input");
+let btn = document.getElementById("button");
+const todo_list = document.getElementById("todo_list");
+
+btn.onclick = function () {
+  if (input.value === "") {
+    alert("You should type something");
+  } else {
+    todo_list.insertAdjacentHTML("beforeend", getTemplate(input.value));
+    return (input.value = "");
+  }
+};
+
+function getTemplate(title) {
+  return `
+  <div class="todo_list_wrapper">
+          <div class="todo_list_value">
+            <input class="todo_list_check" type="radio" />
+            <div class="input_value">${input.value}</div>
+          </div>
+          <div class="todo_list_btns">
+            <button class="todo_list_edit">Edit</button>
+            <button class="todo_list_delete">Delete</button>
+          </div>
+        </div>
+  `;
+}
