9bda89c6127fb49c1566336eb833ed7be68e6cce
bekzhanJS <bekzhanzhumassatov@gmail.com>
2024-03-11 23:00:33 +0500

    Project is done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let input = document.getElementById("input");
 let btn = document.getElementById("button");
 const todo_list = document.getElementById("todo_list");
 
+// add Task
 btn.onclick = function () {
   if (input.value === "") {
     alert("You should type something");
@@ -16,7 +17,7 @@ function getTemplate(title) {
   <div class="todo_list_wrapper">
           <div class="todo_list_value">
             <input class="todo_list_check" type="radio" />
-            <div class="input_value">${input.value}</div>
+            <div class="input_value">${title}</div>
           </div>
           <div class="todo_list_btns">
             <button class="todo_list_edit">Edit</button>
